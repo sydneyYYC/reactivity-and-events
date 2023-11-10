96418ea3740e7afea21f22d4b9333df3e60b9fed
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-09 18:58:46 -0700

    finished nav styles

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -5,14 +5,14 @@ export const navItems = [
 ]
 </script>
 
-<header class="max-w-full bg-amber-700 ">
-  <div class="p-2 mx-2 ">
-    <img class="max-w-[25px] max-h-[25px] hover:bg-amber-400 hover:rounded-full" src="./logo.svg" alt="pot on stove">
+<header class="max-w-full bg-amber-700 flex justify-between items-center">
+  <div class="p-2 mx-2 min-w-[50px] min-h-[30px]">
+    <img class="max-w-[30px] max-h-[30px] hover:bg-amber-400 hover:rounded-full" src="./logo.svg" alt="pot on stove">
   </div>
   <nav class="max-w-fit text-white">
-    <ul class="flex justify-around">
+    <ul class="flex justify-around self-end">
       {#each navItems as {name , href}}
-      <a {href}><li>{name}</li></a>
+      <a {href}><li class="mx-4 mr-2 my-2 hover:bg-amber-400 p-2 hover:rounded-full">{name}</li></a>
       {/each}
     </ul>
   </nav>
